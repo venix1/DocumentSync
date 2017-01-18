ec6fe5cc171b6223c546f55da644aa30aab8ec35
Daniel Green <venix1@gmail.com>
2017-01-18 10:36:38 -0500

    Implementation of FileSystem Document Store

--- FSync/FileSystemDocumentStore.cs
+++ FSync/FileSystemDocumentStore.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 namespace FSync
 {
-	public class FileSystemDocumentStore
+	public class FileSystemDocument : IDocument
+	{
+	}
+
+	public class FileSystemDocumentStore : IDocumentStore
 	{
 		public FileSystemDocumentStore()
 		{
+		}
+	}
+
+	public class FileSystemDocumentWatcher : DocumentWatcher
+	{
+		public FileSystemDocumentWatcher()
+		{
+			var timer = new System.Timers.Timer(5000);
+			timer.AutoReset = true;
+			timer.Elapsed += ProcessQueue;
+			timer.Start();
+
+			FileSystemWatcher watcher = new FileSystemWatcher();
+			watcher.Path = Root.Inode.FullName;
+			watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+			watcher.Filter = "";
+
+			watcher.Changed += new FileSystemEventHandler(OnChanged);
+			watcher.Created += new FileSystemEventHandler(OnChanged);
+			watcher.Deleted += new FileSystemEventHandler(OnChanged);
+			watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+			watcher.EnableRaisingEvents = true;
 		}
 	}
 }
