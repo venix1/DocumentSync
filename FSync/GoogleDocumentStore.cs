cd7b92207b69760ddc4ba0242f6c23422a63f367
Daniel Green <venix1@gmail.com>
2017-01-18 10:36:22 -0500

    Implementation for Google Drive Document Store

--- FSync/GoogleDocumentStore.cs
+++ FSync/GoogleDocumentStore.cs
@@ -1,10 +1,61 @@
 ﻿using System;
 namespace FSync
 {
-	public class GoogleDocumentStore
+	public class GoogleDriveDocument : IDocument
 	{
-		public GoogleDocumentStore()
+	}
+
+	public class GoogleDriveDocumentStore : IDocumentStore
+	{
+		public GoogleDriveDocumentStore()
 		{
+		}
+	}
+
+	public class GoogleDriveDocumentWatcher : DocumentWatcher
+	{
+		private string savedStartPageToke;
+
+		private void Check()
+		{
+			string pageToken = savedStartPageToken;
+			while (pageToken != null) {
+				var request = DriveService.Changes.List(pageToken);
+
+				request.IncludeRemoved = true;
+				request.Fields = "changes(file(id,md5Checksum,mimeType,modifiedTime,name,parents,size,trashed,version),fileId,removed,time),newStartPageToken,nextPageToken";
+				request.Spaces = "drive";
+
+				var changes = request.Execute();
+				foreach (var change in changes.Changes) {
+					EventArgs syncEvent = null;
+
+					if (change.Removed.GetValueOrDefault(false) || change.File.Trashed.GetValueOrDefault(false)) {
+						Console.WriteLine("Removed: {0} {1}", change.FileId, change.File);
+						syncEvent = new DriveDeletedEventArgs(GetUnifiedFile(change.FileId));
+					} else {
+						UnifiedFile unifiedFile = GetUnifiedFileById(change.FileId);
+
+						if (unifiedFile == null)
+							syncEvent = new DriveCreatedEventArgs(unifiedFile);
+						else if (unifiedFile.File.Name != change.File.Name)
+							syncEvent = new DriveRenamedEventArgs(unifiedFile, GetUnifiedFile(change.File));
+						else
+							syncEvent = new DriveChangedEventArgs(unifiedFile);
+					}
+					Console.WriteLine("{0} {1} {2}", syncEvent, change.FileId, (change.File != null) ? change.File.Name : null);
+					SyncQueue.Enqueue(syncEvent);
+				}
+				if (changes.NewStartPageToken != null) {
+					Console.WriteLine("NewStartPageToken: {0}", changes.NewStartPageToken);
+					// Last page, save this token for the next polling interval
+					savedStartPageToken = changes.NewStartPageToken;
+				}
+				if (changes.NextPageToken != null) {
+					Console.WriteLine("NextPageToken: {0}", changes.NextPageToken);
+				}
+				pageToken = changes.NextPageToken;
+			}
 		}
 	}
 }
