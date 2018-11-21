8a64538eb4ccac8094e32f3cdaa62387bb68392d
Daniel G <dgreen@psycho-appz.net>
2018-11-21 15:36:49 -0500

    Use DocumentSync class to perform sync

--- DocumentSync/Program.cs
+++ DocumentSync/Program.cs
@@ -2,21 +2,21 @@
 
 namespace DocumentSync
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			if (args.Length != 2) {
-				Console.WriteLine("Usage: DocumentSync.exe <source> <destination>");
-				return;
-			}
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 2) {
+                Console.WriteLine("Usage: DocumentSync.exe <source> <destination>");
+                return;
+            }
 
-			// TODO: Replace manual creation with factory method and arguments
-			var srcStore = new FileSystemDocumentStore(args[0]);
-			// var srcStore = new GoogleDriveDocumentStore(args[0]);
-			var dstStore = new FileSystemDocumentStore(args[1]);
-			var program = new DocumentSync(srcStore, dstStore);
-			program.Converge();
-		}
-	}
+            // TODO: Replace manual creation with factory method and arguments
+            var srcStore = new FileSystemDocumentStore(args[0]);
+            // var srcStore = new GoogleDriveDocumentStore(args[0]);
+            var dstStore = new FileSystemDocumentStore(args[1]);
+            var program = new DocumentSync(srcStore, dstStore);
+            program.Converge();
+        }
+    }
 }
