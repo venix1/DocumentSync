15a43a720223629f1c7fcd7a50e1e560e656819b
Daniel G <dgreen@psycho-appz.net>
2018-11-21 11:57:44 -0500

    Basic implementation of a MultiValue Dictionary

--- LibDocumentSync/src/MultiValueDictionary.cs
+++ LibDocumentSync/src/MultiValueDictionary.cs
@@ -1,10 +1,31 @@
 ﻿using System;
-namespace DocumentSync.src
+using System.Collections.Generic;
+
+namespace DocumentSync
 {
-	public class MultiValueDictionary
-	{
-		public MultiValueDictionary()
-		{
-		}
-	}
+    public class MultiValueDictionary<TKey,TValue>
+    {
+        Dictionary<TKey,List<TValue>> mValues;
+
+        public MultiValueDictionary()
+        {
+            mValues = new Dictionary<TKey, List<TValue>>();
+        }
+
+        public void Add(TKey key, TValue item)
+        {
+            if (!mValues.ContainsKey(key))
+                mValues.Add(key, new List<TValue>());
+            mValues[key].Add(item);
+        }
+
+        public Dictionary<TKey, List<TValue>>.Enumerator GetEnumerator()
+        {
+            return mValues.GetEnumerator();
+        }
+
+        public bool RemoveAll(TKey key) {
+            return mValues.Remove(key);
+        }
+    }
 }
