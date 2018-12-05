1388b7c6ce52a55edb986344686f8b5287673866
Daniel G <dgreen@psycho-appz.net>
2018-12-05 01:44:40 -0500

    Interface updates
    * Have DocumentException inherit from SystemException
    * Fix Update arguments, so that dst is refelected instead of src
    * Add CopyAttributes to copy just meta data
    * Fix DocumentStore so that primitive methods are abstract and generics are not.

--- LibDocumentSync/src/IDocumentStore.cs
+++ LibDocumentSync/src/IDocumentStore.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace DocumentSync {
-    public class DocumentException : Exception {
+    public class DocumentException : SystemException {
+        public DocumentException(string msg) : base(msg) { }
     }
 
     public class DocumentDoesNotExistException : DocumentException {
+        public DocumentDoesNotExistException(string msg) : base(msg) { }
     }
 
     public enum DocumentType {
@@ -56,6 +59,7 @@ namespace DocumentSync {
     }
 
     public interface IDocumentStore : IEnumerable<IDocument> {
+        IDocument Root { get; }
         IDocument CreateFile(string path, Stream stream);
         IDocument CreateFile(string path, string content);
         // IDocument CreateFile(IDocument parent, string name);
@@ -68,29 +72,55 @@ namespace DocumentSync {
 
         IDocument Create(string path, DocumentType type);
         IDocument Create(IDocument parent, string name, DocumentType type);
-
+        Stream Open(IDocument document, System.IO.FileMode mode);
+        bool Exists(string path);
         void Delete(IDocument arg0);
         void MoveTo(IDocument src, IDocument dst);
         void MoveTo(IDocument src, string name);
 
-        void Update(IDocument src, Stream data);
+        void Update(IDocument dst);
+        void Update(IDocument dst, Stream data);
         void Copy(IDocument src, IDocument dst);
+        void CopyAttributes(IDocument src, IDocument dst);
         IDocument Clone(IDocument src, string dst);
 
         IDocument GetById(string id);
         IDocument GetByPath(string path);
 
         IEnumerable<IDocument> EnumerateFiles(string path = "/", string filter = "*", SearchOption options = SearchOption.AllDirectories);
-        IEnumerable<IDocument> List(IDocument document=null);
+        IEnumerable<IDocument> EnumerateFiles(IDocument path, string filter = "*", SearchOption options = SearchOption.AllDirectories);
+        IEnumerable<IDocument> List(IDocument document = null);
 
         DocumentWatcher Watch();
     }
 
     public abstract class DocumentStore : IDocumentStore {
+        // abstract primitives
+        public abstract IDocument Root { get; }
+        public abstract Stream Open(IDocument document, System.IO.FileMode mode);
         public abstract IDocument Create(string path, DocumentType type);
         public abstract IDocument Create(IDocument parent, string name, DocumentType type);
+
+        public abstract void Update(IDocument dst);
+        public abstract void Delete(IDocument arg0);
+        public abstract IDocument GetById(string id);
+        public abstract IDocument GetByPath(string path);
+
+        public bool Exists(string path) => GetByPath(path) != null;
+        public abstract void MoveTo(IDocument src, IDocument dst);
+        public abstract void MoveTo(IDocument src, string name);
+        public abstract void Update(IDocument dst, Stream stream);
+        abstract public IEnumerable<IDocument> EnumerateFiles(IDocument path, string filter = "*", SearchOption options = SearchOption.AllDirectories);
+
+        public abstract DocumentWatcher Watch();
+
+        public abstract IEnumerator<IDocument> GetEnumerator();
+
+        // generic wrappers
         public IDocument CreateFile(string path, Stream stream) {
             Console.WriteLine("Creating {0}", path);
+            if (Exists(path))
+                throw new DocumentException("File exists");
             var document = Create(path, DocumentType.File);
             document.Update(stream);
             return document;
@@ -99,14 +129,16 @@ namespace DocumentSync {
             var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(content));
             return CreateFile(path, stream);
         }
-        public abstract void Update(IDocument src, Stream stream);
-        public abstract void Copy(IDocument src, IDocument dst);
-        public abstract void Delete(IDocument arg0);
-        public abstract IDocument GetById(string id);
-        public abstract IDocument GetByPath(string path);
-        public abstract void MoveTo(IDocument src, IDocument dst);
-        public abstract void MoveTo(IDocument src, string name);
-        public abstract DocumentWatcher Watch();
+
+        public void CopyAttributes(IDocument src, IDocument dst) {
+            dst.ModifiedTime = src.ModifiedTime;
+        }
+        public void Copy(IDocument src, IDocument dst) {
+            using (var fp = src.OpenRead()) {
+                Update(dst, fp);
+            }
+            CopyAttributes(src, dst);
+        }
 
         public IDocument Clone(IDocument src, string dst) {
             DocumentType docType;
@@ -119,21 +151,21 @@ namespace DocumentSync {
             else { throw new NotImplementedException(); }
             var newDoc = Create(dst, docType);
             Copy(src, newDoc);
-            CopyMetadata(src, newDoc);
+            CopyAttributes(src, newDoc);
             return newDoc;
         }
-        public void CopyMetadata(IDocument src, IDocument dst) {
-            dst.ModifiedTime = src.ModifiedTime;
+
+        public IEnumerable<IDocument> EnumerateFiles(string path = "/", string filter = "*", SearchOption options = SearchOption.AllDirectories) {
+            var document = GetByPath(path);
+            return EnumerateFiles(document, filter, options);
         }
 
-        /** Naive implememntation.  Can be optimized per Store as required **/
-        abstract public IEnumerable<IDocument> EnumerateFiles(string path = "/", string filter = "*", SearchOption options = SearchOption.AllDirectories);
 
         // TODO: Refactor out and use IDocument for implementation
         public IEnumerable<IDocument> GetContents(IDocument document) {
             throw new NotImplementedException();
         }
-        public IEnumerable<IDocument> List(IDocument document=null) {
+        public IEnumerable<IDocument> List(IDocument document = null) {
             /*
              * 			foreach (var document in EnumerateFiles("/", "", SearchOption.AllDirectories)) {
                 yield return document;
@@ -143,8 +175,6 @@ namespace DocumentSync {
             throw new NotImplementedException();
         }
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
-
-        public abstract IEnumerator<IDocument> GetEnumerator();
     }
 
     public enum DocumentChangeType {
@@ -184,8 +214,10 @@ namespace DocumentSync {
 
     public abstract class DocumentWatcher // : System.ComponentModel.Component
     {
+        public Thread PollThread { get; protected set; }
         public abstract DocumentEventArgs Classify(IDocument change);
 
+        public bool PauseRaisingEvents { get; set; }
         public bool EnableRaisingEvents { get; set; }
         public bool IncludeSubdirectories { get; set; }
 
