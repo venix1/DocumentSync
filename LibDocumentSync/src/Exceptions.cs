44ceb97a997f72c72d114b8fc7fe96bca0751aff
Daniel Green <dgreen@psycho-appz.net>
2019-04-14 08:12:37 -0400

    Add some basic DocumentSync exceptions

---
+++ LibDocumentSync/src/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace DocumentSync {
+    public class DocumentException : SystemException {
+        public DocumentException(string msg) : base(msg) { }
+    }
+
+    public class DocumentDoesNotExistException : DocumentException {
+        public DocumentDoesNotExistException(string msg) : base(msg) { }
+    }
+}
